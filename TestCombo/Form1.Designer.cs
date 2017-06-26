5e74ee340fa6633fc45c80ed9b5f9ddc7bf538e2
Trung Mac <trungg.dev@gmail.com>
2017-06-26 15:08:29 +0700

    init prj

---
+++ TestCombo/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TestCombo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_label = new System.Windows.Forms.Label();
+            this.m_combo = new SergeUtils.EasyCompletionComboBox();
+            this.m_methodCB = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // m_label
+            // 
+            this.m_label.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_label.Location = new System.Drawing.Point(12, 76);
+            this.m_label.Name = "m_label";
+            this.m_label.Size = new System.Drawing.Size(333, 13);
+            this.m_label.TabIndex = 2;
+            this.m_label.Text = "Selection:";
+            this.m_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // m_combo
+            // 
+            this.m_combo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_combo.FormattingEnabled = true;
+            this.m_combo.Location = new System.Drawing.Point(12, 42);
+            this.m_combo.Name = "m_combo";
+            this.m_combo.Size = new System.Drawing.Size(333, 21);
+            this.m_combo.TabIndex = 0;
+            this.m_combo.SelectedIndexChanged += new System.EventHandler(this.onSelectionChanged);
+            // 
+            // m_methodCB
+            // 
+            this.m_methodCB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_methodCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.m_methodCB.FormattingEnabled = true;
+            this.m_methodCB.Location = new System.Drawing.Point(110, 12);
+            this.m_methodCB.Name = "m_methodCB";
+            this.m_methodCB.Size = new System.Drawing.Size(235, 21);
+            this.m_methodCB.TabIndex = 3;
+            this.m_methodCB.SelectionChangeCommitted += new System.EventHandler(this.onMethodChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Matching method:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 109);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.m_methodCB);
+            this.Controls.Add(this.m_combo);
+            this.Controls.Add(this.m_label);
+            this.Name = "Form1";
+            this.Text = "EasyCompletionComboBox Test";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.onKeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label m_label;
+        private SergeUtils.EasyCompletionComboBox m_combo;
+        private System.Windows.Forms.ComboBox m_methodCB;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestCombo/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using SergeUtils;
+
+namespace TestCombo
+{
+    public partial class Form1 : Form
+    {
+        private static readonly object[] s_NOBELS_LITTERATURE =
+        {
+            "Albert Camus",
+            "Aleksandr Isayevich Solzhenitsyn",
+            "Alice Munro",
+            "Anatole France",
+            "André Paul Guillaume Gide",
+            "Bjørnstjerne Martinus Bjørnson",
+            "Boris Leonidovich Pasternak",
+            "Carl Friedrich Georg Spitteler",
+            "Carl Gustaf Verner von Heidenstam",
+            "Christian Matthias Theodor Mommsen",
+            "Count Maurice (Mooris) Polidore Marie Bernhard Maeterlinck",
+            "Czeslaw Milosz",
+            "Dario Fo",
+            "Derek Walcott",
+            "Doris Lessing",
+            "Earl (Bertrand Arthur William) Russell",
+            "Elfriede Jelinek",
+            "Elias Canetti",
+            "Erik Axel Karlfeldt",
+            "Ernest Miller Hemingway",
+            "Eugene Gladstone O'Neill",
+            "Eugenio Montale",
+            "Eyvind Johnson",
+            "François Mauriac",
+            "Frans Eemil Sillanpää",
+            "Frédéric Mistral",
+            "Gabriel García Márquez",
+            "Gabriela Mistral",
+            "Gao Xingjian",
+            "George Bernard Shaw",
+            "George Elliot",
+            "Gerhart Johann Robert Hauptmann",
+            "Giorgos Seferis",
+            "Giosuè Carducci",
+            "Grazia Deledda",
+            "Günter Grass",
+            "Halldór Kiljan Laxness",
+            "Harry Martinson",
+            "Harold Pinter",
+            "Heinrich Böll",
+            "Henri Bergson",
+            "Henrik Pontoppidan",
+            "Henryk Sienkiewicz",
+            "Hermann Hesse",
+            "Herta Müller",
+            "Iain Banks",
+            "Imre Kertész",
+            "Isaac Bashevis Singer",
+            "Ivan Alekseyevich Bunin",
+            "Ivo Andric",
+            "Jacinto Benavente",
+            "Jean-Marie Gustave Le Clézio",
+            "Jean-Paul Sartre",
+            "Johannes Vilhelm Jensen",
+            "John Galsworthy",
+            "John M. Coetzee",
+            "John Steinbeck",
+            "José Echegaray y Eizaguirre",
+            "José Saramago",
+            "Joseph Brodsky",
+            "Juan Ramón Jiménez",
+            "Karl Adolph Gjellerup",
+            "Kenzaburo Oe",
+            "Knut Pedersen Hamsun",
+            "Luigi Pirandello",
+            "Mario Vargas Llosa",
+            "Miguel Angel Asturias",
+            "Mikhail Aleksandrovich Sholokhov",
+            "Mo Yan",
+            "Nadine Gordimer",
+            "Naguib Mahfouz",
+            "Nelly Sachs",
+            "Octavio Paz",
+            "Odysseus Elytis",
+            "Orhan Pamuk",
+            "Pablo Neruda",
+            "Pär Fabian Lagerkvist",
+            "Patrick White",
+            "Paul Johann Ludwig Heyse",
+            "Pearl Buck",
+            "Rabindranath Tagore",
+            "Roger Martin du Gard",
+            "Romain Rolland",
+            "Rudolf Christoph Eucken",
+            "Rudyard Kipling",
+            "Saint-John Perse",
+            "Salman Rushdie",
+            "Salvatore Quasimodo",
+            "Samuel Beckett",
+            "Saul Bellow",
+            "Seamus Heaney",
+            "Selma Ottilia Lovisa Lagerlöf",
+            "Shmuel Yosef Agnon",
+            "Sinclair Lewis",
+            "Sir Vidiadhar Surajprasad Naipaul",
+            "Sir Winston Leonard Spencer Churchill",
+            "Sully Prudhomme",
+            "Thomas Stearns Eliot",
+            "Tomas Tranströmer",
+            "Toni Morrison",
+            "Yasunari Kawabata",
+            "Valerie Martin",
+            "Vicente Aleixandre",
+            "William Faulkner",
+            "Wislawa Szymborska",
+            "Wole Soyinka",
+            "Thomas Mann",
+            "Sigrid Undset",
+            "Wladyslaw Stanislaw Reymont",
+            "William Butler Yeats",
+        };
+
+        public class MethodItem
+        {
+            public string Name { get; set; }
+            public StringMatchingMethod Value { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_combo.Items.Add("EBSAntiJitterConfig");
+            m_combo.Items.Add("BridgeFilterConfig2");
+            m_combo.Items.Add("PCMActiveUserList");
+            m_combo.Items.Add("SpotFXPriceConfigIDList");
+            m_combo.Items.AddRange(s_NOBELS_LITTERATURE);
+
+            m_methodCB.DisplayMember = "Name";
+            m_methodCB.Items.Add(new MethodItem { Name = "No wildcards", Value = StringMatchingMethod.NoWildcards });
+            m_methodCB.Items.Add(new MethodItem { Name = "Use wildcards", Value = StringMatchingMethod.UseWildcards });
+            m_methodCB.Items.Add(new MethodItem { Name = "Use regexs", Value = StringMatchingMethod.UseRegexs });
+            m_methodCB.SelectedIndex = 0;
+
+            m_combo.MatchingMethod = StringMatchingMethod.NoWildcards;
+
+            Font = new Font("Segoe UI", 9.0f);
+
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine("key down {0}", ActiveControl.GetType().Name);
+        }
+
+        private void onSelectionChanged(object sender, System.EventArgs e)
+        {
+            m_label.Text = string.Format("Selection: '{0}'", m_combo.SelectedItem);
+        }
+
+        private void onMethodChanged(object sender, System.EventArgs e)
+        {
+            MethodItem mi = (MethodItem) m_methodCB.SelectedItem;
+            m_combo.MatchingMethod = mi.Value;
+        }
+    }
+}
